df764eb642bdc14066a871c12401909551394d4b
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-08-19 07:50:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(kazi = "roller-skate") {
+    return `This Saturday, I want to ${kazi}!`;
+  }
+  
+  const mondayWork = function (kazi = "go to the office") {
+    return `This Monday, I will ${kazi}.`;
+  };
+  
+  function wrapAdjective(string = "*") {
+    return function (feels = "special") {
+      return `You are ${string}${feels}${string}!`;
+    };
+  }
+  wrapAdjective()();
